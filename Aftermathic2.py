3b153153875a2e375581d4ac8b502fc65e9d77fa
Aftermathic <91349435+Aftermathic@users.noreply.github.com>
2021-09-24 17:20:03 -0500

    Update Aftermathic2.py

--- Aftermathic2.py
+++ Aftermathic2.py
@@ -20,14 +20,17 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.startswith('$help'):
+        await message.channel.send('Prefix: $\n\nCommands:\n$help - Description: I will tell you all of the commands and show you what I can do!\n\n$ping - Description: When the command is used, I will reply "Pong!" back to you.\n\n$inspire - Description: I will find a inspiring quote to read to you, hopefully inspiring you, hence the command name.\n\nOther Stuff:\n I will greet someone when they type "hello".')
+
     if message.content.startswith('$ping'):
         await message.channel.send('Pong!')
 
     if message.content.startswith('hello'):
-        await message.channel.send('Hello! I am Aftermathic2.')
+        await message.channel.send('Hello! I am Aftermathic2! If you want to use me my prefix is $.')
 
     if message.content.startswith('$inspire'):
         quote = get_quote()
         await message.channel.send(quote)
 
-client.run('put in your own bot token here')
+client.run('put your own bot token here')
