935354a248f3616f8f51da8724f2783ae9fabcc7
Aftermathic <91349435+Aftermathic@users.noreply.github.com>
2021-09-24 16:32:19 -0500

    Aftermathic2.py
    The main file of the bot.

---
+++ Aftermathic2.py
@@ -0,0 +1,33 @@
+import discord
+import os
+import requests
+import json
+
+client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$ping'):
+        await message.channel.send('Pong!')
+
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello! I am Aftermathic2.')
+
+    if message.content.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
+
+client.run('put in your own bot token here')
